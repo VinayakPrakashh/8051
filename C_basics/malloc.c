83239380005e0abee3b27a625db2e8af930813c9
Vinayak Prakash <vinayakprakash2121@gmail.com>
2025-06-13 16:09:50 +0530

    t

--- C_basics/malloc.c
+++ C_basics/malloc.c
@@ -4,6 +4,8 @@
 
 int main() {    
     int *p = (int *)malloc(5 * sizeof(int)); // Allocate memory for 5 integers
+    int *c = (int *)calloc(5 , sizeof(int)); // Allocate memory for another 5 integers
+    *p = 10; // Assign a value to the first element of the allocated memory
     if (p == NULL) {
         fprintf(stderr, "Memory allocation failed\n");
         return 1; // Exit if memory allocation fails
@@ -11,10 +13,10 @@ int main() {
     else{
         printf("Memory allocated successfully at address: %p\n", (void*)p);
     }
-    printf("content of allocated memory:%d\n", (void*)p);
+    printf("content of allocated memory:%d\n", *p);
     free(p); // Free the allocated memory
+    p = NULL; // Set pointer to NULL after freeing memory to avoid dangling pointer
     printf("Memory freed successfully\n");
-     p = NULL; // Set pointer to NULL after freeing memory
-    printf("content of freed memory:%d\n", (void*)p); // Accessing freed memory is undefined behavior
-   int *p
+    printf("content of freed memory:%d\n", *p); // Accessing freed memory is undefined behavior
+   
 }
